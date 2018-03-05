a0b5239ddede5482a446aef30534e00080811285
1456593200 <https://github.com/1456593200/Imitation-Game.git>
2018-03-05 15:23:40 +0800

    页面无法跳转
    可以连接到bmob数据库，并成功访问数据，但是用户登录成功之后无法进行页面跳转

--- v1.0/Controllers/LoginController.cs
+++ v1.0/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using v1._0.Models;
@@ -27,44 +28,25 @@ namespace v1.Controllers
         }
 
         [HttpPost]
-        public ActionResult LoginIn(string UserName, string UserPwd)
+        public void LoginIn()
         {
+            string UserName = Request["txt_account"];  //获取输入的用户名
+            string UserPwd = Request["txt_password"];  //获取输入的密码
 
-            //var result = new ManagerServices().Login(UserName, UserPwd);
-            //if(result)
-            //{
-            //    return Content(new AjaxResult { state = ResultType.success.ToString(), message = "登录成功。" }.ToJson());
-            //}
-            //else
-            //{
-            //    return Content(new AjaxResult { state = ResultType.error.ToString(), message = "请验证帐号及密码！" }.ToJson());
-            //}
-
-            bool flag;
             BmobUser user = new BmobUser();
             BmobWindows bmob = new BmobWindows();
             bmob.Login<BmobUser>(UserName, UserPwd, (resp, exception) =>
             {
                 if (exception != null)
                 {
-                    flag = false;
                     Debug.WriteLine("登录失败, 失败原因为： " + exception.Message);
                     return;
                 }
-                flag = true;
+                Response.Write("<script>window.location.href='https://www.baidu.com/'</script>");
                 Debug.WriteLine("登录成功, 当前用户对象Session： " + BmobUser.CurrentUser.sessionToken);
 
             });
 
-            if(flag)
-            {
-                return Content(new AjaxResult { state = ResultType.success.ToString(), message = "登录成功。" }.ToJson());
-            }
-            else
-            {
-                return Content(new AjaxResult { state = ResultType.error.ToString(), message = "请验证帐号及密码！" }.ToJson());
-            }
-            
             //try
             //{
             //    if (UserName == "admin")
